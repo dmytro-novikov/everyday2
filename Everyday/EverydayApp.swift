fdb5a969394453d8475d6b01a135a4bdddeca234
dima <dmitry.kulinichev@gmail.com>
2021-03-30 19:20:46 +0300

    Initial Commit

---
+++ Everyday/EverydayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverydayApp.swift
+//  Everyday
+//
+//  Created by dima on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct EverydayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
